55c848e8e109a558b6504e2e37aa66e0f584f863
João Vitor Guimarães <joao_jvga@icloud.com>
2026-05-29 23:59:57 -0300

    feat: adiciona app recipes e configura urls

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+from recipes.views import home, contato, sobre
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
+    path('contato/', contato),
+    path('sobre/', sobre)
 ]
